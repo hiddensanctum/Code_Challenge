a79b6d0ebcb649098869ee2526ce570df070c862
hiddensanctum <hiddensanctum@server.fake>
2013-08-11 22:25:44 -0400

    Added User Manager Rail App

---
+++ user_manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserManager::Application.config.secret_token = '529ee064cf9dc3e42e3742e34f36f2bc237b567f7e1984896dfd4376afb6a60daec77f12f22ab0ba4edd31ac8245288210b379d7e0a5e256c3c7f2c5b76a936d'
